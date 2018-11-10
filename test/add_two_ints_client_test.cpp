16f3af25c31900c7b496647234931d861d42b990
saccharios <saccharios@gmail.com>
2018-11-10 15:49:19 +0100

    Add tutorial files and test files.

---
+++ test/add_two_ints_client_test.cpp
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+#include "ros/ros.h"
+
+TEST(TestSuite, testCase1)
+{
+    int i = 1;
+    int j = 3;
+    EXPECT_EQ(4, i + j);
+}
+
+TEST(TestSuite, testCase12)
+{
+    int i = 1;
+    int j = 3;
+    EXPECT_EQ(6, i + j);
+}
+
+
+int main(int argc, char **argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+//  ros::init(argc, argv, "tester");
+//  ros::NodeHandle nh;
+  return RUN_ALL_TESTS();
+}
