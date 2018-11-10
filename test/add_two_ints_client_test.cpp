c7cc4879083bd89ffd814769a8fb3ff584a659e8
saccharios <saccharios@gmail.com>
2018-11-10 16:40:59 +0100

    Add ros node to test.

--- test/add_two_ints_client_test.cpp
+++ test/add_two_ints_client_test.cpp
@@ -12,14 +12,14 @@ TEST(TestSuite, testCase12)
 {
     int i = 1;
     int j = 3;
-    EXPECT_EQ(6, i + j);
+    EXPECT_EQ(4, i + j);
 }
 
 
 int main(int argc, char **argv)
 {
   testing::InitGoogleTest(&argc, argv);
-//  ros::init(argc, argv, "tester");
-//  ros::NodeHandle nh;
+  ros::init(argc, argv, "tester");
+  ros::NodeHandle node_handle;
   return RUN_ALL_TESTS();
 }
