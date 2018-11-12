0ba9086a4890761bded41949825ac2b3242a633d
saccharios <saccharios@gmail.com>
2018-11-12 21:40:38 +0100

    Trying stuff in the test

--- test/listener_impl_test.cpp
+++ test/listener_impl_test.cpp
@@ -7,7 +7,6 @@ TEST(ListenerTest, Listen)
 
     int argc = 0;
     char **argv = nullptr;
-    ros::init(argc, argv, "tester");
     // Mock talker
     ros::NodeHandle n;
     ros::Publisher talker_pub = n.advertise<std_msgs::String>("talker", 1000);
@@ -15,10 +14,11 @@ TEST(ListenerTest, Listen)
     std::stringstream ss;
     ss << "listener node test";
     expected_msg.data = ss.str();
-    talker_pub.publish(expected_msg);
 
 
     ListenerImpl listener(argc, argv);
+    ros::spinOnce();
+    talker_pub.publish(expected_msg);
     ros::spinOnce();
     std::string actual_msg = listener.getMessage();
     EXPECT_EQ(actual_msg, expected_msg.data);
@@ -30,7 +30,7 @@ int main(int argc, char **argv)
 {
   testing::InitGoogleTest(&argc, argv);
 
-//  ros::init(argc, argv, "tester");
+  ros::init(argc, argv, "tester");
 //  ros::NodeHandle node_handle;
 
   return RUN_ALL_TESTS();
