80e8934694d797da875aa3495337b64bde2f2422
saccharios <saccharios@gmail.com>
2018-11-12 21:40:28 +0100

    Fixed listener node

--- src/listener_node.cpp
+++ src/listener_node.cpp
@@ -2,8 +2,10 @@
 #include "std_msgs/String.h"
 #include "listener_impl.h"
 
+
 int main(int argc, char **argv)
 {
+    ros::init(argc, argv, "listener_node");
     ListenerImpl listener(argc, argv);
     ros::spin();
     return 0;
