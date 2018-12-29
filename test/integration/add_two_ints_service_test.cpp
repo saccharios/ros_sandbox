48e48919125bdc906f710def9e9ea659611618ea
saccharios <saccharios@gmail.com>
2018-12-29 17:41:15 +0100

    Add multiple test cases

--- test/integration/add_two_ints_service_test.cpp
+++ test/integration/add_two_ints_service_test.cpp
@@ -2,19 +2,39 @@
 #include <gtest/gtest.h>
 #include "ros/ros.h"
 
-TEST(AddTwoIntsTest, AddTwoIntsTest)
+class AddTwoIntsTest : public testing::Test
 {
-    ros::NodeHandle n;
-    ros::ServiceClient client = n.serviceClient<beginner_tutorials::AddTwoInts>("add_two_ints");
+public:
+    void run_test(int x, int y)
+    {
+        ros::NodeHandle n;
+        ros::ServiceClient client = n.serviceClient<beginner_tutorials::AddTwoInts>("add_two_ints");
+        ros::service::waitForService("/add_two_ints", -1);
+        beginner_tutorials::AddTwoInts srv;
+        srv.request.a = x;
+        srv.request.b = y;
+        bool success = client.call(srv);
+        EXPECT_TRUE(success);
+        EXPECT_EQ(srv.response.sum, x+y);
+    }
+};
 
-    ros::service::waitForService("/add_two_ints", -1);
 
-    beginner_tutorials::AddTwoInts srv;
-    srv.request.a = 3;
-    srv.request.b = 4;
-    bool success = client.call(srv);
-    EXPECT_TRUE(success);
-    EXPECT_EQ(srv.response.sum, 3+4);
+TEST_F(AddTwoIntsTest, Add_1_0)
+{
+    run_test(1,0);
+}
+TEST_F(AddTwoIntsTest, Add_2_m3)
+{
+    run_test(2,-3);
+}
+TEST_F(AddTwoIntsTest, Add_0_0)
+{
+    run_test(0,0);
+}
+TEST_F(AddTwoIntsTest, Add_9871_197)
+{
+    run_test(9871,197);
 }
 
 int main(int argc, char **argv)
